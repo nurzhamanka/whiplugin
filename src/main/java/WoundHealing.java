3cf335b7f54a59d8257c282b4a40928eb6a419e2
Nurzhan Sakenov <nurzhan.sakenov@nu.edu.kz>
2019-10-16 22:21:36 +0600

    Renamed the plugin.

--- src/main/java/WoundHealing.java
+++ src/main/java/WoundHealing.java
@@ -91,10 +91,16 @@ public class WoundHealing implements Command {
     private <T extends RealType<T>> Img<T> scaleImage(final Img<T> image) {
         
         final Img<DoubleType> dImg = ops.convert().float64(image);
+        
         final Img<DoubleType> normImg = (Img<DoubleType>) ops.run(Normalize.class, dImg, new DoubleType(0.0), new DoubleType(1.0));
+        
         final Img<DoubleType> sqrImg = (Img<DoubleType>) ops.run(SquareImage.class, normImg);
+        
+        
         final Img<DoubleType> reNormImg = (Img<DoubleType>) ops.run(Normalize.class, sqrImg, new DoubleType(0.0), new DoubleType(65535.0));
+        
         final Img<T> fImg = (Img<T>) ops.convert().uint16(reNormImg);
+        
         return fImg;
         //return ImgView.wrap(rai, image.factory());
         
