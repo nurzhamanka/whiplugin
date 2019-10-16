30f82bdad2888e8fd4f356c69ab11573d3676238
Madiyar <madiyar.katranov@nu.edu.kz>
2019-10-16 22:36:38 +0600

    asd

--- src/main/java/GaussianDifference.java
+++ src/main/java/GaussianDifference.java
@@ -1,6 +1,11 @@
 import net.imagej.ops.AbstractOp;
 import net.imagej.ops.Op;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.ImgView;
+import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
@@ -20,12 +25,20 @@ public class GaussianDifference<T extends RealType<T>> extends AbstractOp {
     @Parameter(type = ItemIO.OUTPUT)
     private Img<T> outImg;
 
+    // For scaling the image.
+    @Parameter
+    private OpService ops;
+
     @Override
     public void run() {
         @SuppressWarnings("unchecked")
         final Img<T> newImg = inImg.factory().create(inImg);
         outImg = newImg;
-
-        gauss(sigma, inImg, outImg);
+//
+//        gauss(sigma, inImg, outImg);
+        // We can also blur the channels.
+//        double[] channelSigmas = sigma, sigma, 0
+        final RandomAccessibleInterval<T> res = ops.filter().gauss(inImg, sigma);
+        outImg = ImgView.wrap(res, inImg.factory());
     }
 }
