8f06f921ca7fbac869e97b81f7bd0b464bf8bb22
Nurzhan Sakenov <nurzhan.sakenov@nu.edu.kz>
2020-02-07 16:04:55 +0600

    Worked a bit on implementing the OPERATION nodes
    Signed-off-by: Nurzhan Sakenov <nurzhan.sakenov@nu.edu.kz>

--- src/main/java/graphs/model/Operation.java
+++ src/main/java/graphs/model/Operation.java
@@ -20,4 +20,9 @@ public class Operation implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
